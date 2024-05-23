98e5d33262596f19d4b0fc451d1c7b224b597584
Ozoniuss <qscxzsewqazxcde@gmail.com>
2024-05-23 23:00:57 +0300

    keep going

---
+++ 3.go
@@ -0,0 +1,12 @@
+package main
+
+// first discuss arrays, which are of fixed length.
+
+// then discuss slices, which build on top of arrays. len vs capacity. introduce
+// make.
+
+// discuss maps. (maybe say Keys have to be comparable? they would discover
+// this anyway, and you don't want information overload. Explain perhaps if
+// they ask only.)
+
+// structs
---
+++ 4.go
@@ -0,0 +1,11 @@
+package main
+
+// flow control, if and for. Also introduce range over slice or map.
+
+// range will go over arrays inthe same order
+
+// range will go over map elements in an unspecified order
+
+func main() {
+
+}
